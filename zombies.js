438f9ab316d90186786c3509cd4876b66f7be19e
Mika Dumont <mikakrantman@gmail.com>
2018-01-11 12:27:35 -1000

    zombies up to takeItem

--- zombies.js
+++ zombies.js
@@ -7,7 +7,11 @@
  * @param {string} name     The item's name.
  * @property {string} name
  */
-
+class Item {
+  constructor(name) {
+    this.name = name;
+  }
+}
 
 /**
  * Class => Weapon(name, damage)
@@ -24,7 +28,12 @@
  * @param {number} damage   The weapon's damage.
  * @property {number} damage
  */
-
+class Weapon extends Item {
+  constructor(name, damage) {
+    super(name);
+    this.damage = damage;
+  }
+}
 
 /**
  * Weapon Extends Item Class
@@ -48,7 +57,12 @@
  * @param {number} energy     The energy the food provides.
  * @property {number} energy
  */
-
+class Food extends Item{
+  constructor(name, energy) {
+    super(name);
+    this.energy = energy;
+  }
+}
 
 /**
  * Food Extends Item Class
@@ -79,6 +93,29 @@
  * @property {method} getMaxHealth         Returns private variable `maxHealth`.
  */
 
+class Player extends Item {
+  constructor(name, health, strength, speed) {
+    super(name);
+    this.health = health;
+    this.strength = strength;
+    this.speed = speed;
+    this._name = name;
+    this._health = health; 
+    this._strength = strength;
+    this._speed = speed;
+    this.isAlive = true;
+    this.equipped = false;
+    this._pack = [];
+    this._maxHealth = health;
+    }
+    getPack() {
+      return this._pack;
+    }
+    getMaxHealth() {
+      return this._maxHealth;
+    }
+
+  }
 
 /**
  * Player Class Method => checkPack()
